21066ebc01ca729e6bf8f418ec9e63e56956c7c8
tns <>
2023-07-27 21:55:42 +0900

    タイムラインを表示する！

--- src/pages/mi.vue
+++ src/pages/mi.vue
@@ -2,7 +2,8 @@
 export default{
     data() {
         return {
-            misuki:{}   
+            misuki:{},
+            timeline:{},
         }
     },
     methods: {
@@ -17,12 +18,25 @@ export default{
                 })
             })).json();
             console.log(this.misuki)
+        },
+        async getTimeline() {
+            this.timeline = await (await fetch("https://misskey.systems/api/notes/local-timeline", {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                method : "POST",
+                body : JSON.stringify({
+                    i : "yRbr21ta6LdjewCy6n5fzNbMPcbZo71d",
+                })
+            })).json();
+            console.log("timeline")
         }
+        
     }
 }
 </script>
 <template>
-    <button @click="mi">みすきー！</button>
+    <button @click="mi">わたしの情報</button>
     <div>
         <p>みすてむずのわたしの情報</p>
         <img :src="misuki.avatarUrl" />
@@ -30,4 +44,14 @@ export default{
         <p>{{ misuki.name }}</p>
         <p>{{ misuki.avatarUrl }}</p>
     </div>
+    <button @click="getTimeline">タイムラインを取得！</button>
+    <div> 
+        <div v-for="(item, key) in timeline" :key="key">
+            <img :src="item.user.avatarUrl" style="width: 100px;"/>{{ item.user.name }}
+            <div>
+                {{ item.text }}
+            </div>
+        </div>
+    </div>
+    <pre>{{ timeline }}</pre>
 </template>
