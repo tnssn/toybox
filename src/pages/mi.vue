a024d04fd02e2cab8e24724a1b8deb33a7327cc5
tns <>
2023-08-10 23:11:28 +0900

    ドライブ一覧の取得

--- src/pages/mi.vue
+++ src/pages/mi.vue
@@ -7,38 +7,30 @@ export default{
     },
     data() {
         return {
-            misuki:{},
-            timeline:{},
-            meta: {},
             cli: new Misskey.api.APIClient({
             origin: "https://misskey.systems",
             credential: import.meta.env.VITE_MISSKEY_TOKEN,
             }),
+            meta: {},
+            timeline:{},
+            files:{},
             postContent: "",
         }
     },
     async mounted() {
     this.meta = await this.cli.request("i", { detail: true });
-    this.mi()
     },
     methods: {
-        async mi() {
-            this.misuki = await (await fetch("https://misskey.systems/api/i", {
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                method : "POST",
-                body : JSON.stringify({
-                    i : import.meta.env.VITE_MISSKEY_TOKEN,
-                })
-            })).json();
-        },
         async getTimeline() {
-            this.timeline = await this.cli.request("notes/local-timeline");
+            this.timeline = await this.cli.request("notes/local-timeline",{limit: 20});
             console.log(this.timeline)
         }, 
         async MisskeyPost() {
             await this.cli.request("notes/create", {text:this.postContent});
+        },
+        async getFiles() {
+            this.files = await this.cli.request("drive/files", {limit:12});
+            console.log(this.files)
         }
     }
 }
@@ -46,10 +38,49 @@ export default{
 <template>
     <div>
         <p>みすてむずのわたしの情報</p>
-        <img :src="misuki.avatarUrl" />
+        <img :src="meta.avatarUrl" />
 
-        <p>{{ misuki.name }}</p>
+        <p>{{ meta.name }}</p>
     </div>
+    <hr>
+
+    <v-btn @click="getFiles">ふぁいるをとってみる</v-btn>
+    <!-- {{ files }} -->
+    <!-- 
+    <v-card 
+     variant="outlined"
+     v-for="file in files" 
+     :key="file" 
+      class="mx-auto">
+        <v-img 
+         :src="file.thumbnailUrl"/>
+        <v-card-title>
+            {{ file.name }}
+        </v-card-title>
+    </v-card>
+    ほげえ -->
+
+
+    <v-container fluid>
+      <v-row dense>
+        <v-col 
+        v-for="file in files" 
+        :key="file" 
+        >
+          <v-card>
+            <v-img
+              :src="file.thumbnailUrl"
+              class="align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+              cover
+            >
+            <v-card-title class="text-white" v-text="file.name"></v-card-title>
+            </v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
 
     <hr>
     <v-text-field variant="outlined" v-model="postContent"></v-text-field>
@@ -74,6 +105,9 @@ export default{
 </template>
 
 <style scoped>
+template{
+    grid-template-columns: 800px;
+}
 .timeline-no-soto{
     margin: 1em 0;
 }
